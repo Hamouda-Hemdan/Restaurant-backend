a99bd8e3eb30d43d0289faa5e3f331022c9566e3
Hamouda Elgharabawy <hamouda.hedman55@gmail.com>
2025-01-20 18:45:40 +0700

    Cleaned repository and added .gitignore

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+618181ecc699cd10275a959e571768d26a57dee7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6252ac84343a8e7dfdf158ddbe163fcc5790048a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
