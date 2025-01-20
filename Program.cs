cce0c1f748557921664097d8b4edb92eed82ba30
Hamouda Elgharabawy <hamouda.hedman55@gmail.com>
2025-01-20 19:15:47 +0700

    Implement GetAllDishesAsync

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Repositories;
+using WebApplication1.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+           .LogTo(Console.WriteLine, LogLevel.Information)  // Log SQL queries for debugging
+           .EnableSensitiveDataLogging());
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -15,7 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+builder.Services.AddScoped<DishService>();
+builder.Services.AddScoped<IDishRepository, DishRepository>();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
