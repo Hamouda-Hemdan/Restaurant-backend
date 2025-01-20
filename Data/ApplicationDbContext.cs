15b309092d891b58817fb1759df7626e799e1be9
Hamouda Elgharabawy <hamouda.hedman55@gmail.com>
2025-01-20 18:58:53 +0700

    Create Dishmodel

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,19 @@
-﻿namespace WebApplication1.Data
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using resturant1.Models.Entities;
+using resturant1.Models;
+
+namespace resturant1.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Dish> Dishes { get; set; }
+  
+
     }
 }
--- Models/Entities/Dish.cs
+++ Models/Entities/Dish.cs
@@ -1,6 +1,18 @@
-﻿namespace WebApplication1.Models.Entities
+﻿
+namespace resturant1.Models.Entities
 {
     public class Dish
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        public bool Vegetarian { get; set; }
+        public double Rating { get; set; }
+        public DishCategory Category { get; set; }
+
+
+        public ICollection<Rating> Ratings { get; set; }
     }
 }
