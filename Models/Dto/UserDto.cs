556d0eb394fa896421fdd618df09fbe2886bb650
Hamouda Elgharabawy <93956069+Hamouda-Hemdan@users.noreply.github.com>
2025-01-21 23:22:48 +0700

    Create UserDto.cs

---
+++ Models/Dto/UserDto.cs
@@ -0,0 +1,15 @@
+using resturant1.Models.Entities;
+
+namespace resturant1.Models.Dto
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public Gender Gender { get; set; } 
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
