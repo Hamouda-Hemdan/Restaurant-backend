9bff1f3e6c855f4f3f288cfc8528944a8cfa49c0
Hamouda Elgharabawy <93956069+Hamouda-Hemdan@users.noreply.github.com>
2025-01-22 18:15:43 +0700

    Create PaginationMetadata.cs
    "Created PaginationMetaData class."
    
    "Added page number, size, and total pages properties."
    
    "Implemented constructor for PaginationMetaData."
    
    "Added validation for page number and size."

---
+++ Models/Dto/PaginationMetadata.cs
@@ -0,0 +1,9 @@
+namespace resturant1.Models.DTOs
+{
+    public class PaginationMetadata
+    {
+        public int Size { get; set; }
+        public int Count { get; set; }
+        public int Current { get; set; }
+    }
+}
